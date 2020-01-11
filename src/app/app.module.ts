27205d91e3bf0d4f615f6a6151f65bf86a75393f
Joe Giuffrida <giuffridajoe@gmail.com>
2020-01-11 07:06:34 -0500

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,15 +11,16 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { Routes, RouterModule } from '@angular/router';
+
 
 const appRoutes: Routes = [
-   // localhost:4200/users
    { path: '', component: HomeComponent },
+   // localhost:4200/users
    { path: 'users', component: UsersComponent },
-   // the colon means its a dynamic part of the route
+   // the colon means its a dynamic piece of the route
    { path: 'users/:id/:name', component: UserComponent },
-   { path: 'servers', component: ServersComponent }
+   { path: 'servers', component: ServersComponent },
+   { path: 'servers/:id/edit', component: EditServerComponent }
 ];
 
 @NgModule({
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
+
+  ngOnDestroy(): void {
+    // angular does this for you, but adding your own destroy is a good idea
+    this.paramsSubscription.unsubscribe();
+  }
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.user = {
       // "id" is here because we are adding it to the route in app.module.ts
+      // this approach can be usedd if you know for sure these params will be in the route
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
 
     // params as a property of the route acts as an observable. it is a way to subscribe to an event that might happen in the future to then execute code when it happens, without having to wait for it now
-    this.route.params.subscribe(
+    this.paramsSubscription = this.route.params.subscribe(
       // lambda
       (params: Params) => {
         this.user.id = params['id'];
